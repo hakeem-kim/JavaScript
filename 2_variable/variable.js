234e94c48c610c5dddc97b82120edb296f09330f
hakeem-kim <kimhh2607@gmail.com>
2021-08-10 23:13:06 +0900

    JS: Variable_Null & undefined

--- 2_variable/variable.js
+++ 2_variable/variable.js
@@ -89,3 +89,11 @@ const canRead = true;
 const test = 3 < 1; // false
 console.log(`value: ${canRead}, type: ${typeof canRead}`);
 console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
